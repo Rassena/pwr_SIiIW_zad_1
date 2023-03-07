0678023dc4d71cf414462c24a203d52cf1573e54
Nielsfish <259687@student.pwr.edu.pl>
2023-03-07 20:01:24 +0000

    use black and pylint to reformat code

--- import_data.py
+++ import_data.py
@@ -1,9 +1,19 @@
+"""
+import_data function is importing data from files inside folder data
+
+"""
+
 import os
 
 import pandas as pd
 
 
 def import_data(file_name: str = "connection_graph_new.csv") -> pd.DataFrame:
+    """
+    :param file_name: name of .csv file to import
+    :return: DataFrame
+
+    """
     path = os.path.join("data", file_name)
-    df = pd.read_csv(path, low_memory=False)
-    return df
+    data_frame = pd.read_csv(path, low_memory=False)
+    return data_frame
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import import_data as import_data
+"""
+main.py is starting file and for debug
+"""
+
+from import_data import import_data
 
 if __name__ == "__main__":
-    imported_data = import_data.import_data()
+    imported_data = import_data()
     print(imported_data)
